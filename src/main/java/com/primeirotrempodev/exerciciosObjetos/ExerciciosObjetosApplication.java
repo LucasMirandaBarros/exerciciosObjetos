8622e17f0b999a11f7503bb137a99d675485fa47
LucasMiranda <lucaswespras@gmail.com>
2023-03-30 20:57:40 -0300

    exerciciosobjetos

---
+++ src/main/java/com/primeirotrempodev/exerciciosObjetos/ExerciciosObjetosApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrempodev.exerciciosObjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosObjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosObjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primeirotrempodev/exerciciosObjetos/ExerciciosObjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirotrempodev.exerciciosObjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosObjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
